dee44b206b5c57f3cb6ed8e4bf488947809e5db1
Sebastian <50220355+Esbejay@users.noreply.github.com>
2025-02-03 16:31:16 -0500

    Uploading Home Page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,118 +1,235 @@
-jQuery(window).on('load', function() {
-      
-    
-    // HIDE PRELAODER
-    $(".preloader").addClass("preloader-hidden");
+/**
+* Template Name: DevFolio
+* Updated: Jan 29 2024 with Bootstrap v5.3.2
+* Template URL: https://bootstrapmade.com/devfolio-bootstrap-portfolio-html-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
+  "use strict";
 
-    // SHOW/ANIMATE ANIMATION CONTAINER
-    setTimeout(function(){
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
 
-        $(".hero .animation-container").each(function(){
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
 
-            var e = $(this);
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
 
-            setTimeout(function(){
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
 
-                e.addClass("run-animation");
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
 
-            }, e.data("animation-delay") );
+    if (!header.classList.contains('header-scrolled')) {
+      offset -= 16
+    }
 
-        });
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
 
-    }, 900 );
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
 
-    
-});
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
 
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
 
-jQuery(document).ready(function($) {
-	"use strict";
-    
-    
-    $(window).on('load', function() {
-        
-        // HIDE PRELAODER
-        $(".preloader").addClass("preloader-hidden");
-        
-        // SHOW/ANIMATE ANIMATION CONTAINER
-        setTimeout(function(){
-            
-            $(".hero .animation-container").each(function(){
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
 
-                var e = $(this);
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
 
-                setTimeout(function(){
-                    
-                    e.addClass("run-animation");
-                    
-                }, e.data("animation-delay") );
+      let navbar = select('#navbar')
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
 
-            });
-            
-        }, 900 );
-        
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Intro type effect
+   */
+  const typed = select('.typed')
+  if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
     });
-    
-    
-    // INIT PARALLAX PLUGIN
-    $(".hero .background-content.parallax-on").parallax({
-        scalarX: 24,
-        scalarY: 15,
-        frictionX: 0.1,
-        frictionY: 0.1,
+  }
+
+  /**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+  /**
+   * Testimonials slider
+   */
+  new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Preloader
+   */
+  let preloader = select('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove()
     });
-    
-    
-    // OPEN POPUP SEQUENCE
-    $(".open-popup").click(function(){
-        
-        $(".popup").addClass("show");
-        $(".popup").append('<div class="close-popup backface"></div>');
-        
-    });
+  }
 
-    // CLOSE POPUP SEQUENCE
-    $(document).on('click', '.close-popup', function(){ 
-        
-        $(".popup").removeClass("show");
-        $(".popup .backface").remove();
-        
-    });
-    
-    
-    // AJAX SUBSCRIBE FORM
-    $('.subscribe-form').submit(function() {
+  /**
+   * Initiate Pure Counter 
+   */
+  new PureCounter();
 
-        var postdata = $('.subscribe-form').serialize();
-
-        $.ajax({
-
-            type: 'POST',
-            url: 'C:\laragon\www\subscribe-app\subscribe.php',
-            data: postdata,
-            dataType: 'json',
-            success: function(json) {
-
-                $('.subscribe-form').removeClass("form-error");
-
-                if(json.valid === 0) {
-                    
-                    $('.subscribe-form').addClass("form-error");
-                    
-                } else {
-
-                    $('.subscribe-form').addClass("form-success");
-                    $('.subscribe-form input,.subscribe-form button').val('').prop('disabled', true);
-                    
-                }
-                
-            }
-
-        });
-
-        return false;
-
-    });
-    
-    
-});
+})()
